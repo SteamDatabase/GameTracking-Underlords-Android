7542c780651ff589a20ae824e3133a59b6163571
Wendy <freeman@steamdb.info>
2019-07-24 18:33:55 +0000

    v211 | 10 files | M  android/AndroidManifest.xml, M  android/com/valvesoftware/underlords/BuildConfig.java, M  android/res/values/strings.xml, M  game/dac/pak01_dir.txt, M  game/dac/pak01_dir/scripts/synergies.json, M  game/dac/shaders_mobile_gles_dir.txt, M  game/dac/shaders_mobile_vulkan_dir.txt, M  game/dac/steam.inf, M  manifest_android.json, M  manifest_ios.json

--- android/com/valvesoftware/underlords/BuildConfig.java
+++ android/com/valvesoftware/underlords/BuildConfig.java
@@ -5,6 +5,6 @@ public final class BuildConfig {
     public static final String BUILD_TYPE = "release";
     public static final boolean DEBUG = false;
     public static final String FLAVOR = "";
-    public static final int VERSION_CODE = 2000210;
+    public static final int VERSION_CODE = 2000211;
     public static final String VERSION_NAME = "1.0";
 }
