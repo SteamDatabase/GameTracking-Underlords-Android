530126d6445c3e699b1ec7f2a9393f4c02c48bcf
Wendy <freeman@steamdb.info>
2019-10-30 01:46:37 +0000

    v290 | 102 files | M  Protobufs/gcsdk_gcmessages.proto, M  android/AndroidManifest.xml, M  android/com/valvesoftware/underlords/BuildConfig.java, M  android/lib/arm64-v8a/libclient_strings.txt, M  android/lib/arm64-v8a/libengine2_strings.txt, M  android/lib/arm64-v8a/libserver_strings.txt, M  android/res/values/strings.xml, M  game/core/shaders_android_vulkan_dir.txt, M  game/core/shaders_ios_vulkan_dir.txt, M  game/core/shaders_mobile_gles_dir.txt, M  game/dac/pak01_dir.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_brazilian.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_bulgarian.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_czech.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_danish.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_dutch.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_english.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_finnish.txt, M  game/dac/pak01_dir/resource/localization/dac_abilities_french.t

--- android/com/valvesoftware/underlords/BuildConfig.java
+++ android/com/valvesoftware/underlords/BuildConfig.java
@@ -5,6 +5,6 @@ public final class BuildConfig {
     public static final String BUILD_TYPE = "release";
     public static final boolean DEBUG = false;
     public static final String FLAVOR = "";
-    public static final int VERSION_CODE = 2000284;
+    public static final int VERSION_CODE = 2000290;
     public static final String VERSION_NAME = "1.0";
 }
