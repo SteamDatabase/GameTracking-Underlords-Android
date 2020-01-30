fab0b9ad32c07065b69f0837c6c64ac88d3ef218
Wendy <freeman@steamdb.info>
2020-01-30 22:56:38 +0000

    v371 | 138 files | M  Protobufs/dac_gamemessages.proto, M  android/AndroidManifest.xml, M  android/com/valvesoftware/underlords/BuildConfig.java, M  android/lib/arm64-v8a/libanimationsystem_strings.txt, M  android/lib/arm64-v8a/libclient_strings.txt, M  android/lib/arm64-v8a/libengine2_strings.txt, M  android/lib/arm64-v8a/libhost_strings.txt, M  android/lib/arm64-v8a/libmaterialsystem2_strings.txt, M  android/lib/arm64-v8a/libmeshsystem_strings.txt, M  android/lib/arm64-v8a/libnetworksystem_strings.txt, M  android/lib/arm64-v8a/libparticles_strings.txt, M  android/lib/arm64-v8a/librenderingpipelines_strings.txt, M  android/lib/arm64-v8a/librendersystemvulkan_strings.txt, M  android/lib/arm64-v8a/libresourcesystem_strings.txt, M  android/lib/arm64-v8a/libscenesystem_strings.txt, M  android/lib/arm64-v8a/libschemasystem_strings.txt, M  android/lib/arm64-v8a/libserver_strings.txt, M  android/lib/arm64-v8a/libsoundsystem_strings.txt, M  android/lib/arm64-v8a/libvphysics2_strings.txt, M  android/lib/arm64-v8a/libworldrenderer_strin

--- android/com/valvesoftware/underlords/BuildConfig.java
+++ android/com/valvesoftware/underlords/BuildConfig.java
@@ -5,6 +5,6 @@ public final class BuildConfig {
     public static final String BUILD_TYPE = "release";
     public static final boolean DEBUG = false;
     public static final String FLAVOR = "";
-    public static final int VERSION_CODE = 2000366;
+    public static final int VERSION_CODE = 2000371;
     public static final String VERSION_NAME = "1.0";
 }
